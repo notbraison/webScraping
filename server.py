edd5d71b3cdb5fc9a906b7a9b9b10b88562b253d
notbraison <orinabraison@gmail.com>
2023-06-30 09:21:19 +0300

    at least the page is opening now

--- server.py
+++ server.py
@@ -2,10 +2,15 @@
 #in all champions league matches across time
 #and get the average age
 
-from selenium import webdriver#import the webdriver
+from selenium import webdriver #import the webdriver
+from selenium.webdriver.common.by import By
 
-url ='https://www.transfermarkt.co.uk/kaka/profil/spieler/3366'
+url ='https://en.wikipedia.org/wiki/List_of_European_Cup_and_UEFA_Champions_League_finals'
 
-browser=webdriver.Chrome()
+browser = webdriver.Chrome()
 #at this point i had to find my version of chrome and download a driver for it and put it inside this project folder because i will create a path for it later
-browser.find_element("xpath",'//*[@id="kp-wp-tab-overview"]/div[2]/div/div/div/div/div/div[1]/div/a').click()
+browser.get(url)
+element=browser.find_element(By.CLASS_NAME,"mw-page-title-main")
+print(element)
+
+
